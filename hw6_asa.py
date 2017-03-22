208e3c1dd039bd86329ba7e4d08115247c1d7289
luca-venturi <luca.venturi.92@gmail.com>
2017-03-21 23:55:15 -0400

    hw6.py works

--- hw6_asa.py
+++ hw6_asa.py
@@ -4,30 +4,47 @@ import matplotlib.pyplot as plt
 
 # variables initialization
 
-def C0(x): return complex(2.*np.exp(-np.abs(x)))
-def C1(x): return complex(2.*np.exp(-x**2/2))
-def C2(x): return complex(2.*float(np.abs(x) <= 1))
+def C0(x): return np.exp(-np.abs(x))
+def C1(x): return np.exp(-x**2/2)
+def C2(x): return float(np.abs(x) <= 1)
 
-L = 20
-N = L*20
+L = 50
+N = L*30
 
 x = np.array([complex(i*L/N) for i in range(N)])
 
 # function to estimate covariance
 
+"""
 def estimate_covariance_ft(C_vec):
 	
 	dft_C = np.fft.fft(C_vec)*(L/N)
-	it = 10000
+	it = 1000
 	X = np.zeros((N,it))
 	for i in range(it):
-		phi = []
+		phi = np.zeros((N), dtype=complex)
 		for n in range(N):
 			A = np.random.normal(loc=0.0, scale=1.0)
 			B = np.random.normal(loc=0.0, scale=1.0)
-			phi.append((A+B*1.j)*np.sqrt(L*dft_C[n]*0.5))
-		phi = np.array(phi)
-		X[:,i] = np.real(np.fft.ifft(phi)*(N/L))
+			phi[n] = (A+B*1.j)*np.sqrt(L*dft_C[n]*0.5)
+		X[:,i] = np.real(np.fft.ifft(phi))*(N/L)
+
+	return np.cov(X)[0,:], X[:,0]
+"""
+
+def estimate_covariance_ft_circ(C_vec_tilde):
+	
+	N_tilde = 2*N-2
+	invdft_C_tilde = np.fft.ifft(C_vec_tilde)*N_tilde
+	it = 1000
+	X = np.zeros((N,it))
+	for i in range(it):
+		phi = np.zeros((N_tilde), dtype=complex)
+		for n in range(N_tilde):
+			A = np.random.normal(loc=0.0, scale=1.0)
+			B = np.random.normal(loc=0.0, scale=1.0)
+			phi[n] = (A+B*1.j)*np.sqrt(invdft_C_tilde[n])
+ 		X[:,i] = np.real(np.fft.fft(phi)[:N])/np.sqrt(2*N_tilde)
 
 	return np.cov(X)[0,:], X[:,0]
 
@@ -35,38 +52,40 @@ def estimate_covariance_chol(C_vec):
 	
 	real_cov_matrix = np.zeros((N,N))
 	for n in range(N):
-		real_cov_matrix[n,n:] = np.real(C_vec[n:])*0.5
+		real_cov_matrix[n][n:] = np.real(C_vec[:N-n])*0.5
 		real_cov_matrix[n,n] *= 0.5  
 	real_cov_matrix = (real_cov_matrix + real_cov_matrix.T)
 	K = np.linalg.cholesky(real_cov_matrix)
-	it = 1000
+	it = 10000
 	X = np.zeros((N,it))
 	for i in range(it):
-		phi = []
+		phi = np.zeros((N))
 		for n in range(N):
-			phi.append(np.random.normal(loc=0.0, scale=1.0))
-		phi = np.array(phi)
+			phi[n] = np.random.normal(loc=0.0, scale=1.0)
 		X[:,i] = K.dot(phi)
 
 	return np.cov(X)[0,:], X[:,0]
 
-# C0
+# C1
 
-C0_vec = np.array([C2(x[i]) for i in range(N)])
-	
-X_cov, X_sample = estimate_covariance_ft(C0_vec)
+C_vec = np.array([C1(x[i]) for i in range(N)])
+C_vec_tilde = np.append(C_vec, C_vec[N-2:0:-1])
 
-plt.plot(np.real(x),X_sample)
-plt.show()
-plt.plot(np.real(x),np.real(C0_vec)*0.5,np.real(x),X_cov)
-plt.show()
+# C1: DFT
 
-"""X_cov, X_sample = estimate_covariance_chol(C0_vec)
+X_cov, X_sample = estimate_covariance_ft_circ(C_vec_tilde*2.0)
 
 plt.plot(np.real(x),X_sample)
 plt.show()
-plt.plot(np.real(x),np.real(C0_vec)*0.5,np.real(x),X_cov)
-plt.show()"""
+plt.plot(np.real(x),np.real(C_vec),np.real(x),X_cov)
+plt.show()
+"""
+# C1: Choleski
 
+X_cov, X_sample = estimate_covariance_chol(C_vec)
 
-
+plt.plot(np.real(x),X_sample)
+plt.show()
+plt.plot(np.real(x),np.real(C_vec),np.real(x),X_cov)
+plt.show()
+"""
--- prova.py
+++ prova.py
@@ -2,6 +2,10 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = np.array([1,2])
-b = np.array([3,4])
-print a*b  
+a = np.array([[1,1,2,2],[7,7,3,3]])
+b = np.array([3,4,5,6])
+c = a[1][2:]
+print a
+print a.dot(b)
+print c
+print b[:2]
